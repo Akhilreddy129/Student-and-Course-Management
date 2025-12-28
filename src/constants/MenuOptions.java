7a10f69eb25f6860fa331250c423e04b1054fc05
R. Sai Akhileswar Reddy <147367623+Akhilreddy129@users.noreply.github.com>
2025-12-28 17:22:20 +0530

    Add files via upload

---
+++ src/constants/MenuOptions.java
@@ -0,0 +1,24 @@
+package constants;
+
+public final class MenuOptions {
+    private MenuOptions() {}
+
+    // Student Management
+    public static final int ADD_STUDENT = 1;
+    public static final int VIEW_STUDENTS = 2;
+    public static final int SEARCH_STUDENT = 3;
+    public static final int DEACTIVATE_STUDENT = 4;
+
+    // Course Management
+    public static final int ADD_COURSE = 5;
+    public static final int VIEW_COURSES = 6;
+    public static final int TOGGLE_COURSE_STATUS = 7;
+
+    // Enrollment Management
+    public static final int ENROLL_STUDENT = 8;
+    public static final int VIEW_STUDENT_ENROLLMENTS = 9;
+    public static final int UPDATE_ENROLLMENT_STATUS = 10;
+
+    // Exit
+    public static final int EXIT = 0;
+}
